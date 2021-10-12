84f778628b12cd5cf217b7002927ae78d5ea227a
yobriel Castillo <leex.cm@gmail.com>
2021-10-11 22:40:35 -0400

    agg

--- conversor.py
+++ conversor.py
@@ -1,13 +1,15 @@
 pesos = input("¿Cuantos pesos colombianos quieres cambiar?: ")
 pesos = float(pesos)
-valor_dolar = 3753
-dolares =pesos/valor_dolar
+pesos_dolar = 3753
+dolares=pesos/pesos_dolar
+dolares=round(dolares, 2)
 dolares=str(dolares)
 print("Tienes $" + dolares + "dólares")
 
-
-
-
-
-
-
+bolivares=input("¿Cuantos bolivares quieres cambiar?: ")
+bolivares=float(bolivares)
+bolivares_dolar=5.38
+dolareb=bolivares/bolivares_dolar
+dolareb=round(dolareb, 2)
+dolareb=str(dolareb)
+print("Tienes $" + dolareb + "dólares")
