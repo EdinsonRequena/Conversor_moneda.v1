84cd33001f3aefde4b1ae5a296699fd65a6d5e23
yobriel Castillo <leex.cm@gmail.com>
2021-10-11 22:11:22 -0400

    start

---
+++ conversor.py
@@ -0,0 +1,13 @@
+pesos = input("¿Cuantos pesos colombianos quieres cambiar?: ")
+pesos = float(pesos)
+valor_dolar = 3753
+dolares =pesos/valor_dolar
+dolares=str(dolares)
+print("Tienes $" + dolares + "dólares")
+
+
+
+
+
+
+
